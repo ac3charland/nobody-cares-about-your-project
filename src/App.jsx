91cf29d2eed2e51809b6a428cfe0092b8b9297c4
Alex Charland <ac3charland@gmail.com>
2021-04-30 07:42:18 -0500

    Break index.js file into an App.jsx component for easier top-level API calls and event listeners

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => (
+    <React.StrictMode>
+        <div className='content-wrapper'>
+            Nobody cares!
+        </div>
+    </React.StrictMode>
+)
+
+export default App
